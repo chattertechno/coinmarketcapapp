da01b6670013109e3bf04813ed45cc3d3a55531f
Chatter Tech <reneekrom28@gmail.com>
2020-03-23 16:48:53 +0300

    added _map

--- lib/blocs/crypto/crypto_bloc.dart
+++ lib/blocs/crypto/crypto_bloc.dart
@@ -1,19 +1,39 @@
 import 'dart:async';
+import 'dart:html';
 
 import 'package:bloc/bloc.dart';
+import 'package:blockain/models/coin_model.dart';
+import 'package:blockain/repositories/crypto_repository.dart';
 import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
 
 part 'crypto_event.dart';
 part 'crypto_state.dart';
 
 class CryptoBloc extends Bloc<CryptoEvent, CryptoState> {
+  final CryptoRepository _cryptoRepository;
+
+  CryptoBloc({@required CryptoRepository cryptoRepository})
+   : assert(cryptoRepository != null), 
+   _cryptoRepository = cryptoRepository;
+
+
   @override
-  CryptoState get initialState => CryptoInitial();
+  CryptoState get initialState => CryptoEmpty();
 
   @override
   Stream<CryptoState> mapEventToState(
     CryptoEvent event,
   ) async* {
-    // TODO: implement mapEventToState
+    if (event is AppStarted) {
+      yield* _mapAppStartedToState();
+    } else if (event is RefreshCoins) {
+      yield* _getCoins(coins: []);
+    } else if (event is LoadMoreCoins) {
+      yield* _mapLoadMoreCoinsToState(event);
+    }
   }
+
+
+  
 }
--- lib/blocs/crypto/crypto_state.dart
+++ lib/blocs/crypto/crypto_state.dart
@@ -2,9 +2,32 @@ part of 'crypto_bloc.dart';
 
 abstract class CryptoState extends Equatable {
   const CryptoState();
-}
-
-class CryptoInitial extends CryptoState {
-  @override
+    @override
   List<Object> get props => [];
 }
+
+// initial state when we load the app
+class CryptoEmpty extends CryptoState {}
+
+
+// Fetching coins from our api
+class CryptoLoading extends CryptoState {}
+
+
+// Retrieved coins
+class CryptoLoaded extends CryptoState {
+  final List<Coin> coins;
+
+  const CryptoLoaded({this.coins});
+
+  @override
+  List<Object> get props => [coins];
+
+  @override
+  String toString() => 'CryptoLoaded { coins: $coins }';
+  
+}
+
+
+// if we have error when calling api request
+class CryptoError extends CryptoState {}
